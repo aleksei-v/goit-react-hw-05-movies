79ccb273562c7a83c7bb73f1b2570bd0698beb0a
aleksei-v <aleksey.vasilenko7@gmail.com>
2022-10-11 19:26:13 +0300

    add theme and component Box

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,19 @@
+// import { Box } from "./Box";
+
+
 export const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
+      <div
+        style={{
+          height: '100vh',
+          display: 'flex',
+          justifyContent: 'center',
+          alignItems: 'center',
+          fontSize: 40,
+          color: '#010101'
+        }}
+      >
+        React homework template
+      </div>
   );
 };
